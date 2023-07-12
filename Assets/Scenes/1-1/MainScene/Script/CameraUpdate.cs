f5ecb750eb6e5edb9da9d19cb640617fb5b99f55
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-07-12 18:56:23 +0900

    ジャンプ処理追加途中

---
+++ Assets/Scenes/1-1/MainScene/Script/CameraUpdate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraUpdate : MonoBehaviour
+{
+    //Vector3 playerPos = GameObject.Find("Player(assume)").transform.position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //transform.position = new Vector3(playerPos.x, 1, -10);
+    }
+}
--- Assets/Scenes/1-1/MainScene/Script/PlayerMove.cs
+++ Assets/Scenes/1-1/MainScene/Script/PlayerMove.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-
+    bool jumpNow;
+    public Rigidbody rigid;
+    float jumpPower = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,12 +20,26 @@ public class PlayerMove : MonoBehaviour
         if(Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += new Vector3(0.01f, 0.0f, 0.0f);
-
         }
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position -= new Vector3(0.01f, 0.0f, 0.0f);
-
         }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            //transform.position += new Vector3(0.0f, 0.01f, 0.0f);
+        }
+    }
+
+    void Jump()
+    {
+        if (jumpNow)
+        {
+            return;
+        }
+
+        //rigid.AddForce(transform.up * jumpPower, )
+
+        
     }
 }
