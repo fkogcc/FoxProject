0126572182e413f11f809073488c9df6dea51353
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-07-12 18:04:55 +0900

    プレイヤーの横移動追加

---
+++ Assets/Scenes/1-1/MainScene/Script/PlayerMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(0.01f, 0.0f, 0.0f);
+
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(0.01f, 0.0f, 0.0f);
+
+        }
+    }
+}
