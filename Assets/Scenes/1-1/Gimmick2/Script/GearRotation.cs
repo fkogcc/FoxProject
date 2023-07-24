87ac38378ab61f0df6c2bc365f73f942c6dc24d6
S0624 <mmasyumaro.012345@gmail.com>
2023-07-24 16:45:26 +0900

    ハンドルの修正

--- Assets/Scenes/1-1/Gimmick2/Script/GearRotation.cs
+++ Assets/Scenes/1-1/Gimmick2/Script/GearRotation.cs
@@ -10,55 +10,72 @@ public class GearRotation : MonoBehaviour
     // 回転.
     public Quaternion _rotation;
     // TODO プレイヤーがボタンを押したかのフラグ
-    public bool _pushButton;
+    public bool _playerRptation;
     // TODO プレイヤーがボタンを押したかのフラグ
     public bool _colRange;
-
+    float _gearDegree;
     // インスタンスの作成.
     void Start()
     {
-        // 初期化
-        _rotaDegrees += new Vector3( 0.0f, 1.0f, 0.0f); 
-        _rotation = Quaternion.AngleAxis(0.5f, _rotaDegrees);
-        _pushButton = false;
+        // 初期化.
+        _rotaDegrees += new Vector3(0.0f, 1.0f, 0.0f);
+        _rotation = Quaternion.AngleAxis(0.0f, _rotaDegrees);
+        _playerRptation = false;
         _colRange = false;
         rb = GetComponent<Rigidbody>();
+        _gearDegree = 0.0f;
+
     }
 
     // 60フレームに一回の更新処理.
     void FixedUpdate()
     {
-        if (_pushButton)
+        if (_playerRptation)
         {
-            // 回転度合をかけて足す
+            // 回転度合をかけて足す.
             this.transform.rotation = _rotation * this.transform.rotation;
-            
         }
     }
+
 
     // 更新処理.
     void Update()
     {
-        if (_colRange)
+        if (!_playerRptation)
         {
-            Debug.Log("ボタンをおせる");
-            if (Input.GetKey(KeyCode.KeypadEnter) || Input.GetKey(KeyCode.LeftShift))
+            _gearDegree = this.transform.localEulerAngles.y % 360;
+            if (_gearDegree >= 355.0f)
             {
-                Debug.Log("ボタンをおした");
-                _pushButton = true;
-                rb.freezeRotation = _pushButton;
+                Debug.Log("こえた");
             }
-        }
-        else
-        {
-            Debug.Log("ボタンをおせない");
+
+            if (_colRange)
+            {
+                Debug.Log("ボタンをおせる");
+
+                if (_gearDegree >= 355.0f)
+                {
+                    Debug.Log("一回転した");
+                    _playerRptation = true;
+                    rb.freezeRotation = true;
+
+                    _rotation = Quaternion.AngleAxis(1.5f, _rotaDegrees);
+                }
+            }
+            else
+            {
+                Debug.Log("ボタンをおせない");
+            }
+
+            //Debug.Log(_pushButton);
+
         }
     }
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            // プレイヤーがEmptyのコライダーに入ったとき
+            // プレイヤーがコライダーに入ったとき.
             Debug.Log("範囲内");
             _colRange = true;
             // InspectorタブのonTriggerStayで指定された処理を実行する
@@ -70,7 +87,7 @@ public class GearRotation : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            // プレイヤーがEmptyのコライダーから出たとき
+            // プレイヤーがコライダーから出たとき.
             Debug.Log("範囲外");
             _colRange = false;
         }
