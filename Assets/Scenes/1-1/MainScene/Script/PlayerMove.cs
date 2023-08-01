ed2b2ce50b51cb27084a8b47b4c578c5d17ec0bc
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-08-01 17:00:06 +0900

    プレイヤーの体力追加途中

--- Assets/Scenes/1-1/MainScene/Script/PlayerMove.cs
+++ Assets/Scenes/1-1/MainScene/Script/PlayerMove.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    bool _isJumpNow;// ジャンプしているかどうか
+    private GameObject _player;
     private Rigidbody _rigid;// プレイヤーのリジットボディ
+    private BoxCollider _pMaterial;
+    int _hp;
     float _jumpPower = 25.0f;// ジャンプ力
+    bool _isJumpNow;// ジャンプしているかどうか
+
 
     // Start is called before the first frame update
     void Start()
     {
+        _player = GameObject.Find("Fox0726lam");
         _rigid = GetComponent<Rigidbody>();
+        _pMaterial = GetComponent<BoxCollider>();
+        _hp = 5;
     }
     
     // Update is called once per frame
@@ -21,48 +28,34 @@ public class PlayerMove : MonoBehaviour
         float vert = Input.GetAxis("Vertical");
         float speed = hori * 25.0f;// 速さ
         Vector3 vec = new Vector3(speed, 0, 0);
-        if ((hori != 0) || (vert != 0))
+        // 移動しているかどうかでプレイヤーの摩擦力を変更
+        if ((hori != 0))
         {
-            //Debug.Log("stick:" + hori + "," + vert);
+            _pMaterial.material.dynamicFriction = 0.0f;
         }
+        else if(hori == 0)
+        {
+            _pMaterial.material.dynamicFriction = 1.0f;
+        }
+
+        //Debug.Log(_pMaterial.material.dynamicFriction);
 
         if (hori != 0)
         {
-            //transform.position += new Vector3(hori / 10.0f, 0.0f, 0.0f);
-            // スティックの方向け度合いで速度調整
-            //rigid.transform.position += new Vector3(speed, 0.0f, 0.0f);
-            //rigid.transform.position += new Vector3(0.1f, 0.0f, 0.0f);
-
             // 速度が10以下ならば力を加える
             if(_rigid.velocity.x < 10.0f && _rigid.velocity.x > -10.0f)
             {
                 _rigid.AddForce(vec);
-
-                //rigid.velocity = new Vector3(speed, 0.0f, 0.0f);
             }
             
         }
 
-        //if(rigid.velocity.x >= 10)
-        //{
-        //    rigid.velocity = new Vector3(10.0f,0.0f,0.0f);
-        //}
-
-        //rigid.AddForce(new Vector3(10.0f,0.0f,0.0f));
-
-        Debug.Log(speed);
-
         //Debug.Log(speed);
-        //if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    transform.position -= new Vector3(0.01f, 0.0f, 0.0f);
-        //}
+        
         if (Input.GetKeyDown("joystick button 0"))
         {
-            //transform.position += new Vector3(0.0f, 1.0f, 0.0f);
             Jump();
         }
-        //Debug.Log(jumpNow);
             
 
         //if (Input.GetKeyDown("joystick button 0"))
@@ -116,23 +109,47 @@ public class PlayerMove : MonoBehaviour
         {
             transform.position = new Vector3(-6.0f,1.0f,0.0f);
         }
+
+        if(_hp <= 0)
+        {
+            //_player.active = false;
+
+            this.gameObject.SetActive(false);
+        }
+        Debug.Log(_hp);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         // 地面から離れたら
-        if (_isJumpNow)
+        if (collision.gameObject.tag == "Untagged")
         {
-            _isJumpNow = false;
+            if (_isJumpNow)
+            {
+                _isJumpNow = false;
+            }
         }
+        
     }
 
     private void OnCollisionExit(Collision collision)
     {
         // 地面についたら
-        if (!_isJumpNow)
+        if(collision.gameObject.tag == "Untagged")
         {
-            _isJumpNow = true;
+            if (!_isJumpNow)
+            {
+                _isJumpNow = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 敵に当たったら
+        if(collision.gameObject.name == "Cylinder")
+        {
+            _hp -= 1;
         }
     }
 
