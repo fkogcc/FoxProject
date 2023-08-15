f963e60e5ff09ece1084c99b3271f8399264e9d6
Hmmtrnt <rento5310@gmail.com>
2023-08-16 01:35:52 +0900

    フェード処理追加

--- Assets/Scenes/1-1/MainScene/Script/Scene/TestFade.cs
+++ Assets/Scenes/1-1/MainScene/Script/Scene/TestFade.cs
@@ -2,28 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestFade : MonoBehaviour
 {
-    readonly float waitTime = 1f;
+    
+    private Color _color;
+    private byte _a;// color‚ÌA
 
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
-        StartCoroutine(nameof(Transition));
+        _color = gameObject.GetComponent<Image>().color;
+        _color.r = 0.0f;
+        _color.g = 0.0f;
+        _color.b = 0.0f;
+        _color.a = 1.0f;
+        gameObject.GetComponent<Image>().color = _color;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(_color.a > 0)
+        {
+            _color.a -= 0.01f;
+            gameObject.GetComponent<Image>().color = _color;
+        }
         
-    }
-
-    IEnumerator Transition()
-    {
-        yield return new WaitForSeconds(waitTime);
-
-        Destroy(gameObject);
+        //
     }
 }
