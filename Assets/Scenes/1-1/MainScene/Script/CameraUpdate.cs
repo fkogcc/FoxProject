c6048f2689c55b4872c0802aa19c0f80f8095afe
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-09-06 12:12:32 +0900

    2Dプレイヤーのプレハブ化

--- Assets/Scenes/1-1/MainScene/Script/CameraUpdate.cs
+++ Assets/Scenes/1-1/MainScene/Script/CameraUpdate.cs
@@ -6,6 +6,7 @@ public class CameraUpdate : MonoBehaviour
 {
     //Vector3 playerPos = GameObject.Find("fox").transform.position;
 
+    // プレイヤーのゲームオブジェクト
     GameObject _targetPlayer;
 
     // Start is called before the first frame update
@@ -22,8 +23,16 @@ public class CameraUpdate : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // カメラの追従
+
         float playerPosX = _targetPlayer.transform.position.x;
         float playerPosY = _targetPlayer.transform.position.y;
+        if (transform.position.x <= -10)
+        {
+            transform.position = new Vector3(-10, (playerPosY / 5.0f) + 6.0f, -20.0f);
+        }
+
+        
         transform.position = new Vector3(playerPosX + 7, (playerPosY/5.0f) + 6.0f, -20.0f);
     }
 }
