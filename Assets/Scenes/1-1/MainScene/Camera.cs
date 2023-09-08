b5fe63cb4613aae769bd95bb67a2597092cc2e6a
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-09-08 11:14:31 +0900

    2Dカメラの挙動変更

--- Assets/Scenes/1-1/MainScene/Camera.cs
+++ Assets/Scenes/1-1/MainScene/Camera.cs
@@ -1,3 +1,5 @@
+// 3DƒJƒƒ‰‚Ìˆ—
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Scenes/1-1/MainScene/Script/CameraUpdate.cs
+++ Assets/Scenes/1-1/MainScene/Script/CameraUpdate.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class CameraUpdate : MonoBehaviour
 {
-    //Vector3 playerPos = GameObject.Find("fox").transform.position;
+    // 終着地点座標
+    private Vector3 _targetPosition;
+    
+    private Vector3 _velocity = Vector3.zero;
+
+    [SerializeField] private float _time = 1.0f;
 
     // プレイヤーのゲームオブジェクト
     private GameObject _targetPlayer;
-    // カメラのポジション限界
-    private Vector3 _camearMaxPosition;
+    // カメラのポジション.
     private float _cameraPosX;
     private float _cameraPosY;
     private float _cameraPosZ;
@@ -17,12 +21,13 @@ public class CameraUpdate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // 初期化
         _targetPlayer = GameObject.Find("Foxidle");
 
+        // X,Y座標にプレイヤーの座標を代入
         _cameraPosX = _targetPlayer.transform.position.x;
         _cameraPosY = _targetPlayer.transform.position.y;
         _cameraPosZ = -20.0f;
-        _camearMaxPosition = new Vector3(_cameraPosX + 7, (_cameraPosY / 5.0f) + 6.0f, _cameraPosZ);
     }
 
     // Update is called once per frame
@@ -34,29 +39,38 @@ public class CameraUpdate : MonoBehaviour
     private void FixedUpdate()
     {
         // カメラの追従
+        // プレイヤーの座標を代入し続ける
+        _cameraPosX = _targetPlayer.transform.position.x;
+        _cameraPosY = _targetPlayer.transform.position.y;
 
-        float playerPosX = _targetPlayer.transform.position.x;
-        float playerPosY = _targetPlayer.transform.position.y;
-        if (transform.position.x <= -10)
+        // 向いている方向によってカメラの位置を変更
+        if (!PlayerMove._instance._isDirection)
         {
-            transform.position = new Vector3(-10, (playerPosY / 5.0f) + 6.0f, -20.0f);
-        }
-
-        if(!PlayerMove._instance._isDirection)
-        {
-            _camearMaxPosition = new Vector3(_cameraPosX + 7, (_cameraPosY / 5.0f) + 6.0f, _cameraPosZ);
+            _targetPosition = new Vector3(_cameraPosX + 7, (_cameraPosY / 5.0f) + 6.0f, -20.0f);
         }
         else
         {
-            _camearMaxPosition = new Vector3(_cameraPosX - 7, (_cameraPosY / 5.0f) + 6.0f, _cameraPosZ);
+            _targetPosition = new Vector3(_cameraPosX - 7, (_cameraPosY / 5.0f) + 6.0f, -20.0f);
         }
 
-        if(transform.position.x > _camearMaxPosition.x)
-        {
-            
-        }
+        MoveCamera();
 
+        //DebugCameraPos();
+
+    }
+
+    // デバッグ用カメラポジション
+    private void DebugCameraPos()
+    {
+        float playerPosX = _targetPlayer.transform.position.x;
+        float playerPosY = _targetPlayer.transform.position.y;
         transform.position = new Vector3(playerPosX + 7, (playerPosY / 5.0f) + 6.0f, -20.0f);
-        
+    }
+
+    // カメラの移動
+    private void MoveCamera()
+    {
+        // 最初と最後はゆっくり途中は早くなる移動処理
+        transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref _velocity, _time);
     }
 }
