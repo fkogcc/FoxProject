ad4c8c38916c3f895ce8706fa5790f50cdf0ffaa
Hmmtrnt <rento5310@gmail.com>
2023-09-09 23:21:20 +0900

    風のオブジェクト追加

---
+++ Assets/Scenes/1-1/MainScene/Script/Stage/MoveWind.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWind : MonoBehaviour
+{
+    [SerializeField]
+    private float _windX = 0f;
+    [SerializeField]
+    private float _windY = 0f;
+    [SerializeField]
+    private float _windZ = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// トリガーの範囲に入っている間風の影響を受ける
+    /// </summary>
+    /// <param name="other">当たっている相手</param>
+    private void OnTriggerStay(Collider other)
+    {
+        // 当たったオブジェクトのRigidBody取得
+        Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+
+        // rigidbodyがnullではない時
+        if(rigidbody != null)
+        {
+            // 相手のrigidbodyに力を加える
+            rigidbody.AddForce(_windX, _windY, _windZ, ForceMode.Force);
+        }
+    }
+}
