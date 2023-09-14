3eeb1410e5a4785ead43506b6d203770dca160ef
itasi29 <135081567+itasi29@users.noreply.github.com>
2023-09-14 18:34:35 +0900

    プレイヤーに追従して回転した

--- Assets/Scenes/1-2/Gimick3/Script/BoxPull.cs
+++ Assets/Scenes/1-2/Gimick3/Script/BoxPull.cs
@@ -6,11 +6,21 @@ public class BoxPull : MonoBehaviour
 {
     // プレイヤーの位置情報
     Transform _player;
+    // 子オブジェクト(ギミックのやつ)
+    GameObject _gimmick;
+
+    // 初期の位置
+    Vector3 _startPos;
 
     // 引っ張り始めた位置
-    Vector3 _startPos;
+    Vector3 _pullPos;
     // 距離引っ張り始めた位置から現在までの距離
     Vector3 _delPos;
+
+    // 角度
+    float _angle;
+    Vector3 _arrow;
+    Vector3 _tempPos;
 
     // 引っ張れる範囲にいるかの判定
     bool _isPullRange;
@@ -21,6 +31,14 @@ public class BoxPull : MonoBehaviour
     void Start()
     {
         _player = GameObject.Find("3DPlayer").GetComponent<Transform>();
+
+        _gimmick = transform.GetChild(0).gameObject;
+
+        _startPos = transform.position;
+
+        _angle = 0f;
+        _arrow = new Vector3(0.0f, 1.0f, 0.0f);
+        _tempPos = new Vector3();
 
         _isPullRange = false;
         _isPull = false;
@@ -34,7 +52,7 @@ public class BoxPull : MonoBehaviour
             Debug.Log("引っ張り始めた");
 
             // 引っ張り始めたの位置を保存
-            _startPos = this._player.position;
+            _pullPos = this._player.position;
 
             _isPull = true;
         }
@@ -45,9 +63,10 @@ public class BoxPull : MonoBehaviour
 
             _isPull = false;
 
-            // ここで離した時色が同じならできた処理をする
-            // 現在は元の位置に戻るだけとする
-            this.transform.localScale = Vector3.one;
+            // 元の位置、角度、サイズに戻す
+            _gimmick.transform.position = _startPos;
+            _gimmick.transform.rotation = Quaternion.identity;
+            _gimmick.transform.localScale = Vector3.one;
         }
     }
 
@@ -55,20 +74,25 @@ public class BoxPull : MonoBehaviour
     {
         if (_isPull)
         {
-            _delPos = _startPos - this._player.position;
+            ChangeCenter();
 
-            // 縦、奥行きの大きさを固定
-            _delPos.y = 1f;
-            _delPos.z = 1f;
+            ChangeSize();
 
-            // 右に行かないと長くならないようにする
-            if (-1f < _delPos.x)
+            _tempPos = _player.position;
+            _tempPos.z = _pullPos.z;
+
+            _angle = Vector3.Angle(_tempPos, _player.position);
+
+            if (0 < _player.position.z - _tempPos.z)
             {
-                _delPos.x = 1;
+                _angle *= -1;
             }
 
-            this.transform.localScale = _delPos;
-            Debug.Log("[PullBox]" + this.transform.localScale);
+            //Debug.Log("[PullBox] pull" + _pullPos);
+            //Debug.Log("[PullBox] player" + _player.position);
+            Debug.Log("[PullBox] angle" + _angle);
+
+            this.transform.rotation = Quaternion.AngleAxis(_angle, _arrow);
         }
     }
 
@@ -84,5 +108,37 @@ public class BoxPull : MonoBehaviour
         // 範囲外に出たら引っ張れないようにする
         Debug.Log("引っ張れない");
         _isPullRange = false;
+    }
+
+    /// 　中心を変更する関数(多分これ消す)
+    void ChangeCenter()
+    {
+        // 初めの位置を保存
+        _delPos = _startPos;
+        // 移動している場合中心を右に移動させる
+        _delPos.x = _delPos.x - (_pullPos.x - _player.position.x) / 2.0f;
+
+        // 中心を移動させた物を実装
+        _gimmick.transform.position = _delPos;
+    }
+
+    // サイズを変更する関数(多分これも消す)
+    void ChangeSize()
+    {
+        // 引き始めた位置から現在のX座標の距離を確認
+        _delPos.x = _pullPos.x - this._player.position.x;
+
+        // 右に行かないと長くならないようにする
+        if (-1f < _delPos.x)
+        {
+            _delPos.x = 1;
+        }
+
+        // 縦、奥行きの大きさを固定
+        _delPos.y = 1f;
+        _delPos.z = 1f;
+
+        // サイズの変更
+        _gimmick.transform.localScale = _delPos;
     }
 }
