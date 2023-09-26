976e220de859fb94d2def788283a1f04ff01cd59
S0624 <mmasyumaro.012345@gmail.com>
2023-09-26 15:50:00 +0900

    コードチェック
    優秀

--- Assets/Scenes/1-2/Gimick3/Script/BoxIn.cs
+++ Assets/Scenes/1-2/Gimick3/Script/BoxIn.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class BoxIn : MonoBehaviour
 {
-    // ディレクター
-    BoxDirector _director;
-    // ギミックの色
+    // ディレクター.
+    public BoxDirector _director;
+    // ギミックの色.
     public string Color;
 
+    // 初期化処理.
     void Start()
     {
+        // GimmickDirectorの情報を取得する.
         _director = GameObject.Find("GimmickDirector").GetComponent<BoxDirector>();
     }
 
