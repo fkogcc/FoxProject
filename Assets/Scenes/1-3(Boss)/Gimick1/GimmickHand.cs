87355ab101a64204b78f3ef49ab174592a8f075e
itasi29 <135081567+itasi29@users.noreply.github.com>
2023-09-27 18:48:55 +0900

    コミット

--- Assets/Scenes/1-3(Boss)/Gimick1/GimmickHand.cs
+++ Assets/Scenes/1-3(Boss)/Gimick1/GimmickHand.cs
@@ -4,61 +4,16 @@ using UnityEngine;
 
 public class GimmickHand : MonoBehaviour
 {
-    const float kSpeed = 0.125f;
-
-    public static string HitName;
-
-    Vector3 _vec;
-
-    SlideGimmickDirector _gimmickDirector;
-
-    // 
-    bool _isHitKey;
+    private string HitName;
 
     void Start()
     {
-        HitName = "";
-
-        _vec = new Vector3();
-
-        _gimmickDirector = GameObject.Find("GimmickDirector").GetComponent<SlideGimmickDirector>();
-
-        _isHitKey = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // ˆÚ“®‚ÉŠÖ‚µ‚Ä
-        _vec = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            _vec.x = kSpeed;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            _vec.x = -kSpeed;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            _vec.y = kSpeed;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            _vec.y = -kSpeed;
-        }
-
-        this.transform.position += _vec;
-
-        
+        HitName = "15";
     }
 
     void OnTriggerEnter(Collider other)
     {
         HitName = other.name;
-
-        Debug.Log("[SlideGmmick] nowNum:" + HitName);
     }
 
     public int GetEleNum()
--- Assets/Scenes/1-3(Boss)/Gimick1/SlideGimmickDirector.cs
+++ Assets/Scenes/1-3(Boss)/Gimick1/SlideGimmickDirector.cs
@@ -5,41 +5,44 @@ using UnityEngine;
 
 public class SlideGimmickDirector : MonoBehaviour
 {
+    // プレイヤーの動く量
+    private const float kSpeed = 0.125f;
+
     // 動かすブロックの数.
-    const int kNum = 16;
+    private const int kNum = 16;
 
     // 現在の手の位置からの上下左右
-    const int kUp = -4;
-    const int kDown = 4;
-    const int kLeft = -1;
-    const int kRight = 1;
+    private const int kDirUp = -4;
+    private const int kDirDown = 4;
+    private const int kDirLeft = -1;
+    private const int kDirRight = 1;
 
     // 横に並んでいるブロックの数
-    const int kRaw = 4;
+    private const int kRaw = 4;
 
     // プレイヤー
-    GameObject _player;
+    private GameObject _player;
 
     // ギミック親オブジェ.
-    GameObject _parentObj;
+    private GameObject _parentObj;
     // ギミック子オブジェ.
-    GameObject[] _gimmickObj;
+    private GameObject[] _gimmickObj;
 
     // 要素の番号.
-    int[] _eles;
-
-    // 押したかのフラグ
-    bool _isHitKey;
+    private int[] _eles;
 
     // 現在の要素
-    int _nowEle;
+    private int _nowEle;
 
     // 入れ替え用.
-    Vector3 _tempPos1;
-    Vector3 _tempPos2;
-    int _tempEle;
+    private Vector3 _tempPos1;
+    private Vector3 _tempPos2;
+    private int _tempEle;
 
-    void Start()
+    // クリアしているかしていないか
+    private bool _isCreal;
+
+    private void Start()
     {
         // 初期化
         _player = new GameObject();
@@ -49,13 +52,13 @@ public class SlideGimmickDirector : MonoBehaviour
 
         _eles = new int[kNum];
 
-        _isHitKey = false;
-
-        _nowEle = 0;
+        _nowEle = kNum - 1;
 
         _tempPos1 = new Vector3();
         _tempPos2 = new Vector3();
         _tempEle = 0;
+
+        _isCreal = false;
 
         // プレイヤーを探す
         _player = GameObject.Find("Hand");
@@ -74,54 +77,91 @@ public class SlideGimmickDirector : MonoBehaviour
         // シャッフル
         for(int i = 0; i < 32; i++)
         {
-            // todo: したのを復活
-            //ChangeTrs(Random.Range(0, kNum), Random.Range(0, kNum));
+            // todo: 現状のシャッフルではクリアできない積み配置になってしまうから修正する
+            _nowEle = Random.Range(0, kNum);
+            ChangeTrs(Random.Range(0, kNum));
         }
     }
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.F) && !_isHitKey)
+        // クリアしていたら処理しない
+        if (_isCreal) return;
+
+        // プレイヤーの移動処理
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _player.transform.position += new Vector3(0.0f, kSpeed, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _player.transform.position += new Vector3(0.0f, -kSpeed, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _player.transform.position += new Vector3(kSpeed, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _player.transform.position += new Vector3(-kSpeed, 0.0f, 0.0f);
+        }
+
+        // 特定のボタンを押したらギミックの処理
+        if (Input.GetKeyDown(KeyCode.F))
         {
             _nowEle = _player.GetComponent<GimmickHand>().GetEleNum();
 
             EleCheck();
-
-            _isHitKey = true;
-        }
-        else if (!Input.GetKey(KeyCode.F))
-        {
-            _isHitKey = false;
         }
     }
 
-    public void EleCheck()
+    private void FixedUpdate()
+    {
+        // クリアしていたら処理しない
+        if (_isCreal) return;
+
+        // 要素が番号通りに並んでいるか確認
+        for (int i = 0; i < kNum; i++)
+        {
+            // 要素番号通りでないならここでの処理終了
+            if (_eles[i] != i) return;
+        }
+
+        // ここまで来たらクリアしているので完了とする
+        _isCreal = true;
+        Debug.Log("クリア");
+    }
+
+    private void EleCheck()
     {
         // 要素番号の位置が空白地なら何もしない
         if (_eles[_nowEle] == kNum - 1) return;
 
         // 上のチェック
-        if (DirCheck(kUp)) return;
-        Debug.Log("上違う");
+        if (DirCheck(kDirUp)) return;
         // 下のチェック
-        if (DirCheck(kDown)) return;
-        Debug.Log("下違う");
+        if (DirCheck(kDirDown)) return;
         // 左のチェック
-        if (DirCheck(kLeft)) return;
-        Debug.Log("左違う");
+        if (DirCheck(kDirLeft)) return;
         // 右のチェック
-        if (DirCheck(kRight)) return;
+        if (DirCheck(kDirRight)) return;
     }
 
-    bool DirCheck(int dir)
+    private bool DirCheck(int dir)
     {
+        // 要素数ないにいなければ確認しない
+        if ((_nowEle + dir) < 0 ||
+            kNum <= (_nowEle + dir))
+        {
+            return false;
+        }
         // 左右の場合端にあれば確認しない
-        if (dir == kLeft &&
+        if (dir == kDirLeft &&
             (_nowEle % kRaw) == 0)
         {
             return false;
         }
-        if (dir == kLeft &&
+        if (dir == kDirLeft &&
             (_nowEle & kRaw) == (kRaw - 1))
         {
             return false;
@@ -140,7 +180,7 @@ public class SlideGimmickDirector : MonoBehaviour
 
     /// 位置の変更
     // 要素一つ目、要素二つ目
-    void ChangeTrs(int ele)
+    private void ChangeTrs(int ele)
     {
         // それぞれの位置を保存.
         _tempPos1 = _gimmickObj[_eles[_nowEle]].transform.position;
@@ -154,12 +194,10 @@ public class SlideGimmickDirector : MonoBehaviour
         _tempEle = _eles[_nowEle];
         _eles[_nowEle] = _eles[ele];
         _eles[ele] = _tempEle;
-        
-        // デバッグ表記用
-        Debug.Log("現在の情報");
+
         for (int i = 0; i < 4; i++)
         {
-            Debug.Log(_eles[i * 4] + ", " + _eles[i * 4 + 1] + ", " + _eles[i * 4 + 2] + ", " + _eles[i * 4 + 3] + ", ");
+            Debug.Log(_eles[i * kRaw] + ", " + _eles[i * kRaw + 1] + ", " + _eles[i * kRaw + 2] + ", " + _eles[i * kRaw + 3]);
         }
     }
 }
