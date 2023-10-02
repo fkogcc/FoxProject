794f50c62b6bb79f2ec03d45453ffc02f631881d
ma-si- <Kouma9140@gmail.com>
2023-10-02 18:55:43 +0900

    わけわかめ

--- Assets/Scenes/1-2/Gimick2/PlaneBool.cs
+++ Assets/Scenes/1-2/Gimick2/PlaneBool.cs
@@ -7,9 +7,15 @@ public class PlaneBool : MonoBehaviour
     private bool _isMoving;
 
     private int _MoveAngle;
+
+    private int _lastCol;
     public bool GetMoving() { return _isMoving; }
     public void SetMoving(bool Moving) { _isMoving = Moving; }
     public int GetAngle() { return _MoveAngle; }
     public void SetAngle(int angle) { _MoveAngle = angle; }
+
+    public int GetLastCol() { return _lastCol; }
+    public void SetLastCol(int lastCol) { _lastCol += lastCol; }
+    public void ResetLastCol() { _lastCol = 0; }
 
 }
--- Assets/Scenes/1-2/Gimick2/PlaneManager.cs
+++ Assets/Scenes/1-2/Gimick2/PlaneManager.cs
@@ -13,76 +13,96 @@ public class PlaneManager : MonoBehaviour
         left,
         empty,
     }
-    private GameObject _player;
-    private GameObject _planeManager;
-    private int _moveAngle;
-    private float _planeAngle;
-    public bool _isExit;
-    public bool _isWall;
+    private GameObject _player;//メインプレイヤーをいれる変数
+    private GameObject _planeManager;//移動処理をまとめるスクリプトを入れているオブジェクトを入れる変数
+    private int _moveAngle;//板を踏んだ時の動きの方向を決める変数
+    private float _planeAngle;//板がどの方向を向いてるかを決める変数
+    public bool _isPanelColor;//動き続けるパネルかそうでないかを判断する変数
+    public bool _isWall;//壁かどうかを判断する変数
+    private int _lastCol;//板の処理をいくつ動かしているか判断する変数
 
     private void Start()
     {
-        _player = GameObject.Find("3DPlayer");
-        _planeManager = GameObject.Find("PlaneManager");
-        _planeAngle = this.transform.localEulerAngles.y;
+        _player = GameObject.Find("3DPlayer");//メインプレイヤーを代入する
+        _planeManager = GameObject.Find("PlaneManager");//移動処理をまとめているオブジェクトをいれる
+        _planeAngle = this.transform.localEulerAngles.y;//板の向きを代入する
+
     }
     private void FixedUpdate()
     {
-        if (_planeManager.GetComponent<PlaneBool>().GetMoving())
+        Debug.Log(_planeManager.GetComponent<PlaneBool>().GetLastCol());
+        if (_planeManager.GetComponent<PlaneBool>().GetMoving())//板の処理を動かすかどうかの判断
         {
-            if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.up)
+            if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.up)//板が上を向いている場合
             {
-                _player.transform.position += new Vector3(-0.001f, 0.0f, 0.0f);
+                _player.transform.position += new Vector3(-0.002f, 0.0f, 0.0f);
             }
-            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.right)
+            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.right)//板が右を向いている場合
             {
-                _player.transform.position += new Vector3(0.0f, 0.0f, 0.001f);
+                _player.transform.position += new Vector3(0.0f, 0.0f, 0.002f);
             }
-            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.down)
+            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.down)//板が下を向いている場合
             {
-                _player.transform.position += new Vector3(0.001f, 0.0f, 0.0f);
+                _player.transform.position += new Vector3(0.002f, 0.0f, 0.0f);
             }
-            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.left)
+            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.left)//板が左を向いている場合
             {
-                _player.transform.position += new Vector3(0.0f, 0.0f, -0.001f);
+                _player.transform.position += new Vector3(0.0f, 0.0f, -0.002f);
             }
 
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (!_isWall)
+        //if(_isPanelColor)
+        //{
+        //    _planeManager.GetComponent<PlaneBool>().ResetLastCol();
+        //    _planeManager.GetComponent<PlaneBool>().SetLastCol(1);
+        //}
+        if (!_isWall)//壁出なかったら処理を行う
         {
-            _planeManager.GetComponent<PlaneBool>().SetMoving(true);
-            if (_planeAngle == 0)
+            _planeManager.GetComponent<PlaneBool>().SetLastCol(1);//処理を行っている板の枚数をカウントする
+            _planeManager.GetComponent<PlaneBool>().SetMoving(true);//板の処理を行うようにする
+            if (_planeAngle == 0)//上
             {
                 _planeManager.GetComponent<PlaneBool>().SetAngle((int)moveNum.up);
             }
-            else if (_planeAngle == 90)
+            else if (_planeAngle == 90)//右
             {
                 _planeManager.GetComponent<PlaneBool>().SetAngle((int)moveNum.right);
             }
-            else if (_planeAngle == 180)
+            else if (_planeAngle == 180)//下
             {
                 _planeManager.GetComponent<PlaneBool>().SetAngle((int)moveNum.down);
             }
-            else if (_planeAngle == 270)
+            else if (_planeAngle == 270)//左
             {
                 _planeManager.GetComponent<PlaneBool>().SetAngle((int)moveNum.left);
             }
         }
-        else
+        else//壁にぶつかった場合の処理
         {
-            _planeManager.GetComponent<PlaneBool>().SetMoving(false);
-            _moveAngle = 4;
+            if (_planeManager.GetComponent<PlaneBool>().GetLastCol() > 0)
+            {
+                _planeManager.GetComponent<PlaneBool>().ResetLastCol();
+            }
+            _planeManager.GetComponent<PlaneBool>().SetMoving(false);//板の処理を止める
+            _moveAngle = (int)moveNum.empty;//動きの方向をなくす
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (_isExit)
+        if (_isPanelColor)//黒色のパネルから出た場合
         {
-            _planeManager.GetComponent<PlaneBool>().SetMoving(false);
-            _moveAngle = 4;
+            if (_planeManager.GetComponent<PlaneBool>().GetLastCol() > 0)//処理を行っている板の枚数が1枚以上だった場合
+            {
+                _planeManager.GetComponent<PlaneBool>().SetLastCol(-1);//処理を行っている板の枚数を一枚減らす
+            }
+            if (_planeManager.GetComponent<PlaneBool>().GetLastCol() == 0)//もし処理を行っている枚数がゼロだったら
+            {
+                _planeManager.GetComponent<PlaneBool>().SetMoving(false);//動きを止める
+                _moveAngle = (int)moveNum.empty;//動きの方向をなくす
+            }
         }
     }
 }
