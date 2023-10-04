e8596622ba6ddb2895e114d33f27eb36327843a5
neco0621 <kane263shun@icloud.com>
2023-10-04 18:21:03 +0900

    コンセント以外完成

---
+++ Assets/Scenes/1-3(Boss)/Gimick2/konsent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class konsent : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider konsent)
+    {
+        Debug.Log("ÉNÉäÉA");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+}
---
+++ Assets/Scenes/1-3(Boss)/Gimick2/pisuton.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pisuton : MonoBehaviour
+{
+    // 秒数を数えるカウント.
+    private int _count;
+    //Transformを取得.
+    private Transform _myTransform;
+    // 座標を取得.
+    private Vector3 _pos;
+    // 5秒の時間.
+    private int _time;
+    // ギミックの移動量
+    private float _moveX;
+    void Start()
+    {
+        _count = 0;
+        _myTransform = this.transform;
+        _pos = _myTransform.position;
+        _time = 60;
+        _moveX = 0.08f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _count++;
+
+        // 5秒経ったら.
+        if (_count < _time)
+        {
+            // z座標へ0.08減算.
+            _pos.x -= _moveX;
+            // 座標を設定.
+            _myTransform.position = _pos;
+        }
+        //10秒経ったら.
+        else if (_count < _time * 2)
+        {
+            // z座標へ0.08加算.
+            _pos.x += _moveX;
+            // 座標を設定.
+            _myTransform.position = _pos;
+        }
+        //それ以上になったら.
+        else
+        {
+            //カウントを初期化する.
+            _count = 0;
+        }
+    }
+}
