52b8529a46949d6586b6520744600992e3554eeb
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-10-06 10:22:45 +0900

    二つの道を作成
    残り二つは明日する

--- Assets/Scenes/1-3(Boss)/MainScene/Script/AthleticGimmick/WidthMove.cs
+++ Assets/Scenes/1-3(Boss)/MainScene/Script/AthleticGimmick/WidthMove.cs
@@ -7,13 +7,14 @@ public class WidthMove : MonoBehaviour
     
     private Rigidbody _rigidBody;
 
-    private Vector3 _speed = new Vector3(1.0f, 0.0f, 1.0f);
-
-    private Vector3 _minus = new Vector3(-1, -1, -1);
+    [SerializeField] private Vector3 _speed = new Vector3(1.0f, 0.0f, 1.0f);
 
     List<Rigidbody> _rigidBodies = new();
 
-    // 移動向きが変わるフレーム数
+    // 折り返すときの最大フレーム数
+    [SerializeField] private int _TurningMaxFrame = 0;
+
+    // 移動しているフレーム数
     private int _DirectionChangeFrame = 0;
 
     // Start is called before the first frame update
@@ -51,7 +52,8 @@ public class WidthMove : MonoBehaviour
     private void MoveObject()
     {
         _rigidBody.MovePosition(transform.position + Time.fixedDeltaTime * _speed);
-        if(_DirectionChangeFrame >= 300)
+
+        if(_DirectionChangeFrame >= _TurningMaxFrame)
         {
             _speed = new Vector3(_speed.x * -1, _speed.y * -1, _speed.z * -1);
             _DirectionChangeFrame = 0;
