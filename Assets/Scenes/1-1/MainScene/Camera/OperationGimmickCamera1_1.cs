4c466f9eb8bdb99bbe81daf9c9503e04cf111e50
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-10-16 18:42:05 +0900

    届いたオブジェクト追加

---
+++ Assets/Scenes/1-1/MainScene/Camera/OperationGimmickCamera1_1.cs
@@ -0,0 +1,28 @@
+﻿// ギミックが作動中時のカメラの挙動
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperationGimmickCamera1_1 : MonoBehaviour
+{
+    // ギミックの作動中のカメラの座標.
+    [SerializeField] private GameObject[] _CameraPosition;
+    // ギミックの真偽を統括しているオブジェクト名.
+    [SerializeField] private string _GimmickManagerName;
+
+    // ギミックの真偽を統括しているオブジェクト.
+    private GameObject _GimmickManger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _GimmickManger = GameObject.Find(_GimmickManagerName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
