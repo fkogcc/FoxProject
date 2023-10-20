88d26c1374c169f12285a1a744141ecd6ae7a944
sayuGitHub <114457066+sayudesu@users.noreply.github.com>
2023-10-20 18:49:41 +0900

    sa

---
+++ Assets/Scenes/1-2/Gimick4/Script/Gimick1_2_4_Manager1Mk2.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gimick1_2_4_Manager1Mk2 : MonoBehaviour
+{
+
+    // ボタン操作.
+    GameObject _botton;
+    // 回転用.
+    [SerializeField] GameObject[] _rota;
+    // 判定用.
+    [SerializeField] GameObject[] _coll;
+    // 回転するオブジェクトの最大数.
+    public int _objRotaMaxNum;
+    // 謎解きがとけたかどうか.
+    private bool _isClear = false;
+
+    void Start()
+    {
+        // ボタン用.
+        _botton = GameObject.Find("GameManager2");
+    }
+
+    void Update()
+    {
+        for(int i = 0; i < _objRotaMaxNum; i++)
+        {
+            // オブジェクトにあたっていたら.
+            if (_coll[i].GetComponent<MyCollsion3D>().IsGetHit())
+            {
+                // ボタンをおしたら.
+                if (_botton.GetComponent<Botton>().GetButtonB())
+                {
+                    // 回転したら.
+                    _rota[i].GetComponent<TurnGraph>().Rota();
+                }
+            }
+        }
+
+        // すべての謎がとけたら.
+        if (_rota[0].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[1].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[2].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[3].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[4].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[5].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[6].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[7].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[8].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[9].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[10].GetComponent<TurnGraph>().IsGetAns() &&
+            _rota[11].GetComponent<TurnGraph>().IsGetAns())
+        {
+            // ここでシーンを切り替え.
+        }
+
+    }
+}
