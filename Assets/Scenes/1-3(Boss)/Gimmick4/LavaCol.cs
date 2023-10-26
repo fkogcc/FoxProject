431b4c3d669259af79f13c92fc99fe0598f2f156
ma-si- <Kouma9140@gmail.com>
2023-10-26 19:01:57 +0900

    落ちる床のスクリプト追加

---
+++ Assets/Scenes/1-3(Boss)/Gimmick4/LavaCol.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LavaCol : MonoBehaviour
+{
+    Vector3 kjumpForce = new Vector3(0,1500.0f,0);
+    Rigidbody _rb;
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _rb = other.GetComponent<Rigidbody>();
+            _rb.AddForce(kjumpForce,ForceMode.Impulse);
+        }
+    }
+}
