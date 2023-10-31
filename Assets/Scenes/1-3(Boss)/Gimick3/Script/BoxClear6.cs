72bfec4d8dbb73aeb0d3a3b76147f4b427dcafeb
neco0621 <kane263shun@icloud.com>
2023-10-31 18:58:54 +0900

    2ステージ目の整理

---
+++ Assets/Scenes/1-3(Boss)/Gimick3/Script/BoxClear6.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxClear6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "pCube58")
+        {
+            Debug.Log("クリア4");
+        }
+    }
+}
---
+++ Assets/Scenes/1-3(Boss)/Gimick3/Script/BoxClear7.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxClear7 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "pCube57")
+        {
+            Debug.Log("クリア4");
+        }
+    }
+}
