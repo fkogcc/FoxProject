428481b24c8427806a8fd93d3ca8740cf9bf7125
Hmmtrnt <rento5310@gmail.com>
2023-11-13 02:28:43 +0900

    繋げられる分は繋げた

--- Assets/Scenes/1-2/Gimick3/Script/SceneTransition1_2_3.cs
+++ Assets/Scenes/1-2/Gimick3/Script/SceneTransition1_2_3.cs
@@ -5,43 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition1_2_3 : MonoBehaviour
 {
+    private BoxDirector _boxDirector;
+
     private FadeScene _fade;
 
     // 解いたかどうか.
     private bool _active = false;
 
+    // Start is called before the first frame update
     void Start()
     {
+        _boxDirector = GameObject.Find("GimmickDirector").GetComponent<BoxDirector>();
         _fade = GameObject.FindWithTag("Fade").GetComponent<FadeScene>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (_boxDirector.GetResult())
         {
-            Debug.Log("a");
             _fade._isFadeOut = true;
         }
+
         if (_fade.GetAlphColor() >= 0.9f && _fade._isFadeOut)
         {
-            _active = true;
+            _active = _boxDirector.GetResult();
             SceneManager.sceneLoaded += GameSceneLoaded;
             SceneManager.LoadScene("MainScene1-2");
         }
+    }
 
-        // シーン切り替え時に呼ぶ.
-        void GameSceneLoaded(Scene next, LoadSceneMode mode)
-        {
-            // 切り替え後のスクリプト取得.
-            SolveGimmickManager solveGimmickManager = GameObject.FindWithTag("GimmickManager").GetComponent<SolveGimmickManager>();
-            Player2DMove player2D = GameObject.FindWithTag("Player").GetComponent<Player2DMove>();
+    // シーン切り替え時に呼ぶ.
+    private void GameSceneLoaded(Scene next, LoadSceneMode mode)
+    {
+        // 切り替え後のスクリプト取得.
+        SolveGimmickManager solveGimmickManager = GameObject.FindWithTag("GimmickManager").GetComponent<SolveGimmickManager>();
+        Player2DMove player2D = GameObject.FindWithTag("Player").GetComponent<Player2DMove>();
 
-            // ギミックを解いたかのデータを渡す.
-            solveGimmickManager._solve[2] = _active;
-            player2D.transform.position = new Vector3(235.0f, 0.0f, 0.0f);
+        // ギミックを解いたかのデータを渡す.
+        solveGimmickManager._solve[2] = _active;
+        player2D.transform.position = new Vector3(22.0f, 0.0f, 0.0f);
 
-            // 削除
-            SceneManager.sceneLoaded -= GameSceneLoaded;
-        }
+        // 削除
+        SceneManager.sceneLoaded -= GameSceneLoaded;
     }
 }
