de47c1f420b95b055390df9143aa31b19569a31c
neco0621 <kane263shun@icloud.com>
2023-11-14 18:10:44 +0900

    リセットボタン完成

--- Assets/Scenes/1-3(Boss)/Gimick3/Script/Reset_Button.cs
+++ Assets/Scenes/1-3(Boss)/Gimick3/Script/Reset_Button.cs
@@ -10,10 +10,10 @@ public class Reset_Button : MonoBehaviour
     public GameObject _stage2_Green_Container;
     public GameObject _stage2_Yellow_Container;
 
-    public Vector2 _stage2_Blue_InitialPosition;
-    public Vector2 _stage2_Red_InitialPosition;
-    public Vector2 _stage2_Green_InitialPosition;
-    public Vector2 _stage2_Yellow_InitialPosition;
+    public Vector3 _stage2_Blue_InitialPosition;
+    public Vector3 _stage2_Red_InitialPosition;
+    public Vector3 _stage2_Green_InitialPosition;
+    public Vector3 _stage2_Yellow_InitialPosition;
     //public GameObject _stage2_Red_Container;
 
     // Start is called before the first frame update
@@ -29,16 +29,14 @@ public class Reset_Button : MonoBehaviour
     {
         if (collision.gameObject.name == "3DPlayer")
         {
+
             if (Input.GetKeyDown("joystick button 2"))
             {
                 _stage2_Blue_Container.transform.position = _stage2_Blue_InitialPosition;
-                Debug.Log("Blue");
                 _stage2_Red_Container.transform.position = _stage2_Red_InitialPosition;
-                Debug.Log("Red");
                 _stage2_Green_Container.transform.position = _stage2_Green_InitialPosition;
-                Debug.Log("Green");
                 _stage2_Yellow_Container.transform.position = _stage2_Yellow_InitialPosition;
-                Debug.Log("Yellow");
+                
             }
         }
     }
