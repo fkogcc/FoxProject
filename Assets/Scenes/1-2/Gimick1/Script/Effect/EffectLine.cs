f3e66d484ff041cf6f2f3512c22bfec02bea8ff4
S0624 <mmasyumaro.012345@gmail.com>
2023-11-14 10:54:55 +0900

    こみっと

---
+++ Assets/Scenes/1-2/Gimick1/Script/Effect/EffectLine.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectLine : MonoBehaviour
+{
+    [SerializeField] private GameObject Line = default;
+
+    private GameObject _effectLine;
+
+    private LineRenderer _lineRenderer;
+
+    private List<Vector3>_pointList = new List<Vector3>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        LineInit();
+    }
+    private void LineInit()
+    {
+        // とりあえず 今の位置に生成！
+
+        // 初期化処理
+        _effectLine = Instantiate(Line,this.transform.position,
+                                    Quaternion.identity);
+        // LineRenderer取得
+        _lineRenderer = _effectLine.GetComponent<LineRenderer>();
+        
+        _pointList.Add(this.transform.position);
+
+        _lineRenderer.positionCount = _pointList.Count;
+
+        // 各頂点の座標設定
+        for (int iCnt = 0; iCnt < _pointList.Count; iCnt++)
+        {
+            _lineRenderer.SetPosition(iCnt, _pointList[iCnt]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
