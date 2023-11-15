25e6bea90974bda3f37560215324a5fab0b6f52f
sayuGitHub <114457066+sayudesu@users.noreply.github.com>
2023-11-15 18:02:15 +0900

    ad

---
+++ Assets/Scenes/1-3(Boss)/MainScene/Script/Road3/ObjectRotationToPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotationToPlayer : MonoBehaviour
+{
+    public GameObject _player;
+    private bool _isHit = false;
+   
+    void Update()
+    {
+        if(_isHit)
+        {
+            //_player.transform.parent = gameObject.transform;
+            _player.transform.SetParent(gameObject.transform);
+            _isHit = false;
+
+        }
+        else
+        {
+            _player.transform.parent = null;
+        }
+
+        Debug.Log(_isHit);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _isHit = true;  
+        }
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        _isHit = true;
+    //    }
+    //}
+
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        _isHit = false;
+    //    }
+    //}
+}
