b876ad24c3cd1e829b107c05bc7eb50e59a71c4a
S0624 <114457614+S0624@users.noreply.github.com>
2023-11-19 17:22:04 +0900

    たくさん変更
    ねむいです

---
+++ Assets/Scenes/1-2/Gimick1/Script/Effect/TextManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextManager : MonoBehaviour
+{
+    // 32フレームで消えるように.
+    const float kAlpha = 0.03125f;
+
+    // 色を入れるよう.
+    private Color _color;
+
+    private void Start()
+    {
+        _color = gameObject.GetComponent<Text>().color;
+        _color.a = 1f;
+    }
+
+    private void FixedUpdate()
+    {
+        _color.a -= kAlpha;
+        gameObject.GetComponent<Text>().color = _color;
+
+        if (_color.a < 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scenes/1-3(Boss)/Gimmick4/Scripts/BourstEffectPlay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class BourstEffectPlay : MonoBehaviour
+{
+    [SerializeField] private GameObject _bourst = default;
+    private MoveWall _wall;
+    private GameObject _effectBurst;
+    // エフェクトの再生が終わったかどうか
+    private bool _isEffectNowPlay = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _wall = GameObject.Find("MoveWallDir").GetComponent<MoveWall>();
+        //// エフェクトを生成
+        //_effectBurst = Instantiate(_bourst, this.transform.position,
+        //                                        Quaternion.identity); // 初期化処理
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_wall.GetResult())
+        {
+            if (_effectBurst == null)
+            {
+                // エフェクトを生成
+                _effectBurst = Instantiate(_bourst, this.transform.position,
+                                                        Quaternion.identity); // 初期化処理
+            }
+            else
+            {
+                //  エフェクトの再生が終わっていたらclear判定にする
+                if (!_effectBurst.GetComponent<ParticleSystem>().isPlaying)
+                {
+                    // エフェクトの中身を全部消す.
+                    Destroy(_effectBurst);
+                    _isEffectNowPlay= true;
+                }
+            }
+        }
+    }
+}
