2c6f72b301244f443b2e3db50389179724c21797
sayuGitHub <114457066+sayudesu@users.noreply.github.com>
2023-11-20 16:14:26 +0900

    こうもとくんのへやのコードを修正と自分の部屋のテクスチャを修正

--- Assets/Scenes/1-2/Gimick2/PlaneManager.cs
+++ Assets/Scenes/1-2/Gimick2/PlaneManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlaneManager : MonoBehaviour
 {
     // 移動パターン.
-    private enum moveNum
+    private enum MoveNum
     {
         up,
         right,
@@ -15,17 +15,34 @@ public class PlaneManager : MonoBehaviour
         empty,
     }
 
+    private struct MoveAngle
+    {
+
+        public float up;
+        public float right;
+        public float down;
+        public float left;
+        public float empty;
+    }
+
+
+    static readonly float AutoMoveSpeed = 0.002f;
+    static readonly string PlayerNameTag = "Player";
+
     // プレイヤーオブジェクト.
     private GameObject _player;
     // プレイヤーマネージャーオブジェクト.
     private GameObject _planeManager;
-    // プレイヤーの角度.
-    private float _planeAngle;
-    // 緑の床.
+    // 床オブジェクトの角度.
+    private float _tileObj;
+    // 床オブジェクトから離れた場合移動をやめる.
     public bool _isExit;
+    // 床オブジェクトから離れたかの判定を取る.
     private bool _isExitHit = false;
-    // 青の床.
+    // 壁に当たるまで移動を続ける.
     public bool _isWall;
+    // 角度を代入.
+    MoveAngle _moveAngle;
 
     private void Start()
     {
@@ -34,16 +51,24 @@ public class PlaneManager : MonoBehaviour
         _planeManager = GameObject.Find("PlaneManager");
 
         // プレイヤー角度代入
-        _planeAngle = this.transform.localEulerAngles.y;
+        _tileObj = this.transform.localEulerAngles.y;
+
+        _moveAngle.up = 0;
+        _moveAngle.right = 90;
+        _moveAngle.down = 180;
+        _moveAngle.left = 270;
     }
 
     private void Update()
     {
+        // 現在の床から離れた場合の処理.
         if(_isExitHit)
         {
             // ギミックの発動を停止する.
             _planeManager.GetComponent<PlaneBool>().SetMoving(false);
-            _planeManager.GetComponent<PlaneBool>().SetAngle(4);
+            // 角度を初期化する.
+            _planeManager.GetComponent<PlaneBool>().SetAngle((int)MoveNum.empty);
+            // 床から離れた場合の処理を初期化する.
             _isExitHit = false;
         }
     }
@@ -53,38 +78,41 @@ public class PlaneManager : MonoBehaviour
         // ギミックのが発動したなら.
         if (_planeManager.GetComponent<PlaneBool>().GetMoving())
         {
+            // ユーザーがプレイヤーを動かせなくする.
             _player.GetComponent<Player3DMove>()._isController = false;
             // プレイヤーの移動パターンを見て.
             // 自動で移動を開始.
-            if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.up)
+            if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)MoveNum.up)
             {
-                _player.transform.position += new Vector3(-0.002f, 0.0f, 0.0f);
+                _player.transform.position += new Vector3(-AutoMoveSpeed, 0.0f, 0.0f);
             }
-            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.right)
+            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)MoveNum.right)
             {
-                _player.transform.position += new Vector3(0.0f, 0.0f, 0.002f);
+                _player.transform.position += new Vector3(0.0f, 0.0f, AutoMoveSpeed);
             }
-            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.down)
+            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)MoveNum.down)
             {
-                _player.transform.position += new Vector3(0.002f, 0.0f, 0.0f);
+                _player.transform.position += new Vector3(AutoMoveSpeed, 0.0f, 0.0f);
             }
-            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)moveNum.left)
+            else if (_planeManager.GetComponent<PlaneBool>().GetAngle() == (int)MoveNum.left)
             {
-                _player.transform.position += new Vector3(0.0f, 0.0f, -0.002f);
+                _player.transform.position += new Vector3(0.0f, 0.0f, -AutoMoveSpeed);
             }
         }
         else
         {
+            // ユーザーがプレイヤーを動かせるようにする.
             _player.GetComponent<Player3DMove>()._isController = true;
-            _planeManager.GetComponent<PlaneBool>().SetAngle(4);
+            // 角度を初期化する.
+            _planeManager.GetComponent<PlaneBool>().SetAngle((int)MoveNum.empty);
         }
     }
     private void OnTriggerStay(Collider other)
     {
         // ふれているタグがプレイヤーだった場合.
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == PlayerNameTag)
         {
-            // 青い床では無かった場合.
+            // 壁に当たるまでの処理ではない場合(青い床では無かった場合).
             if (!_isWall)
             {
                 // 仕掛けを発動するようにする.
@@ -92,21 +120,21 @@ public class PlaneManager : MonoBehaviour
 
                 // プレイヤーの角度を見る.
                 // 進行方向を決める.
-                if (_planeAngle == 0)
+                if ((int)_tileObj == (int)_moveAngle.up)
                 {
-                    _planeManager.GetComponent<PlaneBool>().SetAngle((int)moveNum.up);
+                    _planeManager.GetComponent<PlaneBool>().SetAngle((int)MoveNum.up);
                 }
-                else if (_planeAngle == 90)
+                else if ((int)_tileObj == (int)_moveAngle.right)
                 {
-                    _planeManager.GetComponent<PlaneBool>().SetAngle((int)moveNum.right);
+                    _planeManager.GetComponent<PlaneBool>().SetAngle((int)MoveNum.right);
                 }
-                else if (_planeAngle == 180)
+                else if ((int)_tileObj == (int)_moveAngle.down)
                 {
-                    _planeManager.GetComponent<PlaneBool>().SetAngle((int)moveNum.down);
+                    _planeManager.GetComponent<PlaneBool>().SetAngle((int)MoveNum.down);
                 }
-                else if (_planeAngle == 270)
+                else if ((int)_tileObj == (int)_moveAngle.left)
                 {
-                    _planeManager.GetComponent<PlaneBool>().SetAngle((int)moveNum.left);
+                    _planeManager.GetComponent<PlaneBool>().SetAngle((int)MoveNum.left);
                 }
             }
             else
@@ -120,9 +148,9 @@ public class PlaneManager : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         // ふれているタグがプレイヤーだった場合.
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == PlayerNameTag)
         {
-            // 緑の床だった場合.
+            // 緑の床だった場合緑の床から離れた場合の処理をする.
             if (_isExit)
             {
                 _isExitHit = true;
