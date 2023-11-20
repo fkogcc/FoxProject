d8ca3c26f192571846280176a72f0256a95909cd
neco0621 <kane263shun@icloud.com>
2023-11-20 18:58:30 +0900

    エフェクト追加

--- Assets/Scenes/1-3(Boss)/Gimick3/Script/Stage1_Yellow_Container.cs
+++ Assets/Scenes/1-3(Boss)/Gimick3/Script/Stage1_Yellow_Container.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Stage1_Yellow_Container : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _particleRed;
+    [SerializeField] private ParticleSystem _particleGreen;
+    [SerializeField] private ParticleSystem _particlePurple;
     void OnCollisionEnter(Collision collision)
     {
+
         if (collision.gameObject.name == "stage1_kontena_yellow")
         {
             ContainerDirector._count++;
-            Debug.Log("クリア4");
+            EffectPlay();
         }
+    }
+    private void EffectPlay()
+    {
+        _particleRed.Play();
+        _particleGreen.Play();
+        _particlePurple.Play();
     }
 }
