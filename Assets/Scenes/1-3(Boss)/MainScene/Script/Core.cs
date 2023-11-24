b3f03bc4f6aac27bd1976f2cd8d9429f9da4db90
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-11-24 13:31:48 +0900

    大体の部屋の内装を決定

--- Assets/Scenes/1-3(Boss)/MainScene/Script/Core.cs
+++ Assets/Scenes/1-3(Boss)/MainScene/Script/Core.cs
@@ -1,9 +1,12 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Core : MonoBehaviour
 {
+    public GameObject[] _coraFilm;
+    private SolveGimmickManager _gimmickManager;
+
     // Start is called before the first frame update
     void Start()
     {
