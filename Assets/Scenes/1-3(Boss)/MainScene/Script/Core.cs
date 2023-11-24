e7e0c01cd3a2fe6d448e23f5b116466ec9e12657
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-11-24 14:11:15 +0900

    fadeのバグを消去

--- Assets/Scenes/1-3(Boss)/MainScene/Script/Core.cs
+++ Assets/Scenes/1-3(Boss)/MainScene/Script/Core.cs
@@ -4,18 +4,59 @@ using UnityEngine;
 
 public class Core : MonoBehaviour
 {
+    // コア.
+    public GameObject[] _cora;
+    // コアの膜.
     public GameObject[] _coraFilm;
+    // ギミックマネージャー.
     private SolveGimmickManager _gimmickManager;
 
-    // Start is called before the first frame update
     void Start()
+    {
+        _gimmickManager = GameObject.FindWithTag("GimmickManager").GetComponent<SolveGimmickManager>();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        // ギミックを解いたら膜を消し、コアを少し下げる
+        //if(_gimmickManager._solve[0])
+        //{
+        //    Destroy(_coraFilm[0]);
+        //}
+        //if (_gimmickManager._solve[1])
+        //{
+        //    Destroy(_coraFilm[1]);
+        //}
+        //if (_gimmickManager._solve[2])
+        //{
+        //    Destroy(_coraFilm[2]);
+        //}
+        //if (_gimmickManager._solve[3])
+        //{
+        //    Destroy(_coraFilm[3]);
+        //}
+
+        for (int coraNum = 0; coraNum < _cora.Length; coraNum++)
+        {
+            CoraGoDown(coraNum);
+        }
+    }
+
+    // クリアした時のコアの挙動
+    private void CoraGoDown(int coraNum)
+    {
+        if (!_gimmickManager._solve[coraNum] ) return;
+
+        Destroy(_coraFilm[coraNum]);
+        if (_cora[coraNum].transform.position.y > 0)
+        {
+            _cora[coraNum].transform.position += new Vector3(0.0f, -0.05f, 0.0f);
+        }
         
     }
 }
