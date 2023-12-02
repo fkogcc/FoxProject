199cfc297db0bf9051d4ff7e151f450697387350
itasi29 <135081567+itasi29@users.noreply.github.com>
2023-12-03 01:11:05 +0900

    手の調整
    制限位置
    制限の変数変更
    速度調整

--- Assets/Scenes/1-3(Boss)/Gimick1/Script/GimmickHand.cs
+++ Assets/Scenes/1-3(Boss)/Gimick1/Script/GimmickHand.cs
@@ -7,11 +7,11 @@ using UnityEngine;
 public class GimmickHand : MonoBehaviour
 {
     // 手の動く速度
-    private const float kSpeed = 0.125f;
+    private const float kSpeed = 0.09375f;
     // 上下左右の上限
-    private const float kRightLimit = 9.0f;
-    private const float kLeftLimit = -8.0f;
-    private const float kUpLimit = 12.5f;
+    private const float kRightLimit = 8.0f;
+    private const float kLeftLimit = -6.4f;
+    private const float kUpLimit = 12.0f;
     private const float kDownLimit = 2.0f;
 
     private int _hitNo;
@@ -20,12 +20,16 @@ public class GimmickHand : MonoBehaviour
     private float _horizontal;
     private float _vertical;
 
+    private Vector3 _limitPos;
+
     public int HitNo { get { return _hitNo; } }
 
     void Start()
     {
         _hitNo = 15;
         _directer = GameObject.Find("GameManager");
+
+        _limitPos = transform.position;
     }
 
     void OnTriggerEnter(Collider other)
@@ -42,41 +46,42 @@ public class GimmickHand : MonoBehaviour
         // 水平方向.
         _vertical = Input.GetAxis("Vertical");
 
-        // プレイヤーの移動処理.
-        if (0.0f < _horizontal)
+        // プレイヤーの手の移動処理.
+        if (_horizontal != 0.0f)
         {
-            transform.position += Vector3.right * kSpeed;
+            transform.position += Vector3.right * kSpeed * _horizontal;
             if (transform.position.x > kRightLimit)
             {
-                transform.position =
-                    new Vector3(kRightLimit, transform.position.y, transform.position.z);
+                _limitPos.x = kRightLimit;
+                _limitPos.y = transform.position.y;
+
+                transform.position = _limitPos;
             }
-        }
-        if (_horizontal < 0.0f)
-        {
-            transform.position += Vector3.left * kSpeed;
             if (transform.position.x < kLeftLimit)
             {
-                transform.position = 
-                    new Vector3(kLeftLimit, transform.position.y, transform.position.z);
+                _limitPos.x = kLeftLimit;
+                _limitPos.y = transform.position.y;
+
+                transform.position = _limitPos;
             }
         }
-        if (0.0f < _vertical)
+
+        if (_vertical != 0.0f)
         {
-            transform.position += Vector3.up * kSpeed;
+            transform.position += Vector3.up * kSpeed * _vertical;
             if (transform.position.y > kUpLimit)
             {
-                transform.position =
-                    new Vector3(transform.position.x, kUpLimit, transform.position.z);
+                _limitPos.x = transform.position.x;
+                _limitPos.y = kUpLimit;
+
+                transform.position = _limitPos;
             }
-        }
-        if (_vertical < 0.0f)
-        {
-            transform.position += Vector3.down * kSpeed;
             if (transform.position.y < kDownLimit)
             {
-                transform.position =
-                    new Vector3(transform.position.x, kDownLimit, transform.position.z);
+                _limitPos.x = transform.position.x;
+                _limitPos.y = kDownLimit;
+
+                transform.position = _limitPos;
             }
         }
     }
