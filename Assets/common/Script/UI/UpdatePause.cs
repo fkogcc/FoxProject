8b28efd0abcc8d7a53ac4266faa5646ab70c36ea
sayuGitHub <114457066+sayudesu@users.noreply.github.com>
2023-12-04 14:43:37 +0900

    説明を追加

--- Assets/common/Script/UI/UpdatePause.cs
+++ Assets/common/Script/UI/UpdatePause.cs
@@ -9,10 +9,18 @@ public class UpdatePause : MonoBehaviour
     // ポーズ画面のオブジェクト
     [SerializeField] private GameObject _pauseCanvas;
 
-    // Start is called before the first frame update
+    // 説明用クラス
+    private TipsDrawer _tipsDrawer;
+
+    // 現在のタイムスケールを取得する
+    public float _timeScale { get;  set; }
+    
+    // ボタンを押したかどうか
+    private bool _isPressB = false;
+
     void Start()
     {
-        
+        _tipsDrawer = GameObject.Find("Tips").GetComponent<TipsDrawer>();
     }
 
     // Update is called once per frame
@@ -27,18 +35,38 @@ public class UpdatePause : MonoBehaviour
             Time.timeScale = 1;
         }
 
+        // Start
         if (Input.GetKeyDown("joystick button 7"))
         {
             _isPause = true;
         }
+        // B
         if (Input.GetKeyDown("joystick button 1"))
         {
-            _isPause = false;
+            // 説明が出ている場合
+            if (_tipsDrawer._isSlideEnd)
+            {
+                _tipsDrawer.IsUpSlider();
+                _isPressB = true;
+            }
+            else
+            {
+                _isPause = false;
+            }
+        }
+        // 時間がとまっている場合
+        if (_isPause)
+        {
+            // 画像表示
+            if (Input.GetKeyDown("joystick button 3"))
+            {
+                _tipsDrawer.IsDownSlider();
+            }
+
         }
 
-        
-
-        
+        // タイムスケールの取得
+        _timeScale = Time.timeScale;  
     }
 
     private void FixedUpdate()
