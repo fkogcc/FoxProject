e0f76327255e6d174dc73e3aff289b2e9dcc6cf8
Hmmtrnt <rento5310@gmail.com>
2023-12-05 23:39:08 +0900

    ボタン押す処理を追加してみたけど無理だった

--- Assets/Scenes/1-3(Boss)/Gimmick2/Script/Stage_Clear_Switch.cs
+++ Assets/Scenes/1-3(Boss)/Gimmick2/Script/Stage_Clear_Switch.cs
@@ -14,13 +14,33 @@ public class Stage_Clear_Switch : MonoBehaviour
     void OnCollisionStay(Collision collision)
     {
         //プレイヤーが触れたとき.
-        if (collision.gameObject.name == "3DPlayer")
+        //if (collision.gameObject.name == "3DPlayer")
+        //{
+        //    if(Input.GetKeyDown("joystick button 2"))
+        //    {
+        //        //ステージクリアの関数を呼ぶ
+        //        _isClear= true;
+        //    }                            
+        //}
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //プレイヤーが触れたとき.
+        if (other.gameObject.name == "3DPlayer")
         {
-            if(Input.GetKeyDown("joystick button 2"))
+            Debug.Log("当たっている");
+            if (Input.GetKeyDown("joystick button 2"))
             {
-                //ステージクリアの関数を呼ぶ
-                _isClear= true;
-            }                            
+                // ステージクリアの関数を呼ぶ
+                _isClear = true;
+
+                Debug.Log(_isClear);
+            }
+            else
+            {
+                Debug.Log("押していない");
+            }
         }
     }
 
