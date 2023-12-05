9b483eb4b85b77577615053c4a6d9b80c6bfa6ba
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-12-05 14:04:25 +0900

    1-1の表ステージの調整

--- Assets/Scenes/1-1/Gimmick1/Scripot/SceneTransition1_1_1.cs
+++ Assets/Scenes/1-1/Gimmick1/Scripot/SceneTransition1_1_1.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+﻿/*クリアしたらシーン遷移*/
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneTransition1_1_1 : MonoBehaviour
 {
+    // ギミックをクリアした情報を取得.
     private Gimick1_1_1Manager _Gimmick1_1_1;
-    private FadeScene _fade;
+    // フェード処理.
+    private Fade _fade;
+    // フェード管理.
+    private FadeAnimDirector _fadeDirector;
+    // プレイヤー.
     private Player3DMove _Player3D;
 
     // 解いたかどうか.
@@ -17,7 +21,8 @@ public class SceneTransition1_1_1 : MonoBehaviour
     void Start()
     {
         _Gimmick1_1_1 = GetComponent<Gimick1_1_1Manager>();
-        _fade = GameObject.FindWithTag("Fade").GetComponent<FadeScene>();
+        _fade = GameObject.Find("FadeCanvas").GetComponent<Fade>();
+        _fadeDirector = GameObject.Find("Manager").GetComponent<FadeAnimDirector>();
         _Player3D = GameObject.FindWithTag("Player").GetComponent<Player3DMove>();
     }
 
@@ -26,10 +31,10 @@ public class SceneTransition1_1_1 : MonoBehaviour
     {
         if(_Gimmick1_1_1.GetResult())
         {
-            _fade._isFadeOut = true;
+            //_fade._isFadeOut = true;
         }
 
-        if(_fade.GetAlphColor() >= 0.9f && _fade._isFadeOut)
+        //if(_fade.GetAlphColor() >= 0.9f && _fade._isFadeOut)
         {
             _active = _Gimmick1_1_1.GetResult();
             SceneManager.sceneLoaded += GameSceneLoaded;
